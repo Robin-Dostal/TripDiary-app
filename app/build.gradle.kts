05cfd7f7de4fd1012c9b1ee447d7fbe66e42a7a2
r_dostal <r_dostal@utb.cz>
2024-11-21 10:45:57 +0100

    Binding include and MongoDB Atals connection test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,12 @@ android {
     namespace = "com.example.traveldiary"
     compileSdk = 34
 
+    packaging{
+        resources {
+            excludes += "META-INF/native-image/**"
+        }
+    }
+
     buildFeatures {
         viewBinding = true
     }
@@ -35,11 +41,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -49,7 +56,25 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    //implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
+    //implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+
+    //mongodb
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.2.0")
+    implementation("org.mongodb:bson-kotlinx:5.2.0")
+    implementation ("org.mongodb:mongodb-driver-sync:4.10.0")
+    implementation ("org.litote.kmongo:kmongo:4.9.0")
+
+    implementation ("org.slf4j:slf4j-simple:2.0.9")
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.7.3")
+    implementation ("io.projectreactor:reactor-core:3.5.11")
+    implementation ("org.jetbrains.kotlin:kotlin-reflect:1.9.10")
+
+    //implementation("org.mongodb:mongodb-driver-sync:4.10.0")
 }
--- app/src/main/java/com/example/traveldiary/MainActivity.kt
+++ app/src/main/java/com/example/traveldiary/MainActivity.kt
@@ -1,17 +1,38 @@
 package com.example.traveldiary
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.traveldiary.databinding.ActivityMainBinding
+import com.example.traveldiary.databinding.ToolbarBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbarBinding = ToolbarBinding.bind(binding.mytoolbar.root)
+        toolbarBinding.toolbarTitle.text = "Home"
+
+
+        // Initialize the Spinner with some sample data
+        val filterOptions = arrayOf("Option 1", "Option 2", "Option 3")
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, filterOptions)
+        adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        binding.filterDropdown.adapter = adapter // Use binding to access the Spinner
+
+        // Initialize the Search Bar
+        binding.searchBar.setOnClickListener {
+            // You can add functionality for the search bar here
+        }
     }
 }
